b92c962984bb72959e0e56f3c3db7de31d9cd82c
Cristian Rodriguez Benitez <126631418+CritianRodriguez042502@users.noreply.github.com>
2023-11-03 18:06:12 -0500

    Agregando la vista para seleccionar noticias por categoria

--- apps/api_bbc_news/views.py
+++ apps/api_bbc_news/views.py
@@ -1,11 +1,13 @@
 from django.http import JsonResponse, HttpResponse
+from rest_framework.views import APIView
 import requests
 
-apiKey = "da09c7a07669433f86613d78bc8721ea";
-url = f'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={apiKey}';
+apiKey = "da09c7a07669433f86613d78bc8721ea"
 
 
 def news (request) :
+    url = f'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={apiKey}'
+    
     if request.method == "GET":
         res = requests.get(url=url)
         if res.status_code == 200 :
@@ -19,8 +21,16 @@ def news (request) :
 
 def newsByCategory (request) :
     if request.method == "GET":
-        return JsonResponse(
-            {"blogs" : "por categoria"}
-        )
+        params = request.GET.get("slug")
+        
+        url = f'https://newsapi.org/v2/everything?q=${params}&language=es&from=2023-10-02&sortBy=publishedAt&apiKey={apiKey}'
+        
+        res = requests.get(url=url)
+        
+        if res.status_code == 200 and params :
+            return JsonResponse({"data" : res.json().get("articles")})
+        
+        else :
+            return JsonResponse({"Error" : str(409)})
     else :
         return HttpResponse(f"El metodo '{request.method}' no esta permitido")
